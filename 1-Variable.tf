ba79ff451ae6bf3e073d248c7f0580159943d448
jdiph051sj <90809185+jdiph051sj@users.noreply.github.com>
2024-05-21 22:55:46 +0200

    Create 1-Variable.tf

---
+++ 1-Variable.tf
@@ -0,0 +1,13 @@
+#Declaring Input Variables
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+
+}
+
+#variable "key_name" {
+#  description = "Key to access the EC2 instance"
+#  type        = string
+#  default     = "key_pair"
+#}
